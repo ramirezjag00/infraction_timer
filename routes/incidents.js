77d77210a92d9d66807b3e320cd184f15128d211
Andrey <33412680+ramirezjag00@users.noreply.github.com>
2018-03-07 17:40:44 +0800

    infraction_timerv11

--- routes/incidents.js
+++ routes/incidents.js
@@ -2,25 +2,43 @@
 const express = require("express");
 const router = express.Router();
 const Incident = require("../models/incident");
-// const Deliverable = require("../models/deliverable");
+const Deliverable = require("../models/deliverable");
+// const Comment = require("../models/comment");
 const User = require("../models/user");
 const middleware = require("../middleware");
 
 //GET ROUTE
 
 
-router.get("/", middleware.isLoggedIn, function(req,res) {
-	//get all incidents from DB
-
-	Incident.find({}, function(err, allIncidents) {
-
-		if (err){ 
-			console.log(err);
-		} else {
-			res.render("incidents/index", {incidents: allIncidents});
-		}
-	});
+router.get("/", middleware.isLoggedIn, function(req,res){
+	var noMatch = null;
+	if(req.query.search){
+		const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+		Incident.find({title: regex}, function(err, allIncidents){
+			User.find({}, function(err, allUsers){
+			if(err){
+				res.redirect('back');
+			} else {
+				if(allIncidents.length === 0){
+					noMatch = "'"+req.query.search+"'"+ " did not match incidents";
+				}
+				res.render("incidents/index", {incidents:allIncidents, users: allUsers, noMatch:noMatch});
+			}
+			});         
+		});
+	} else {
+		Incident.find({}, function(err, allIncidents){
+			User.find({}, function(err, allUsers){
+				if(err){
+					res.redirect('back');
+				} else {
+					res.render("incidents/index", {incidents:allIncidents, users: allUsers, noMatch:noMatch});
+				}
+			});
+		});
+	}
 });
+
 
 //CREATE ROUTE
 router.post("/", middleware.isLoggedIn, function(req,res) {
@@ -28,6 +46,7 @@ router.post("/", middleware.isLoggedIn, function(req,res) {
 	const status = req.body.status;
 	const description = req.body.description;
 	const owner = req.body.owner;
+	const deadline = req.body.deadline;
 	const createdBy = {
 		id: req.user._id,
 		username: req.user.username,
@@ -35,12 +54,14 @@ router.post("/", middleware.isLoggedIn, function(req,res) {
 		lname: req.user.lname
 	};
 	
-	const newIncident = {title: title, status: status, description: description, owner: owner, createdBy: createdBy}
+	const newIncident = {title: title, status: status, description: description, owner: owner, createdBy: createdBy, deadline: deadline}
 	//create a new incident and save to DB
 	Incident.create(newIncident, function(err, newlyCreated) {
 		if (err) {
-			console.log(err);
+			req.flash('error', 'Incident could not be created');
+			res.redirect('back');
 		} else {
+			req.flash('success', 'Incident was added');
 			res.redirect("/incidents");
 		} 
 		
@@ -48,55 +69,71 @@ router.post("/", middleware.isLoggedIn, function(req,res) {
 });
 
 // NEW ROUTE
-// router.get("/new", middleware.isLoggedIn, (req,res) => res.render("incidents/new"));
 
-router.get("/new", middleware.isLoggedIn, function(req,res) {
-	User.find({}, function(err, allUsers){
-		if(err) {  
-			console.log(err);
+// router.get("/new", middleware.isLoggedIn, function(req,res) {
+// 	User.find({}, function(err, allUsers){
+// 		if(err) {  
+// 			console.log(err);
+// 		} else {
+// 			res.render("incidents/new", {users: allUsers});
+// 		}
+// 	});
+// });
+
+//SHOW - RESTFUL ROUTE
+router.get("/:id", middleware.checkIncidentOwnership, function(req,res) {
+	//find the Incident with the provided ID
+	Incident.findById(req.params.id).populate("deliverables").exec(function(err,foundIncident){
+		if(err){
+			req.flash('error', 'Incident was not found');
+			res.redirect('back');
 		} else {
-			res.render("incidents/new", {users: allUsers});
+			res.render("incidents/show", {incident: foundIncident});	
 		}
 	});
 });
 
-//SHOW - RESTFUL ROUTE
-router.get("/:id", middleware.isLoggedIn, function(req,res) {
-	//find the Incident with the provided ID
-	Incident.findById(req.params.id, function(err,foundIncident){
-		if(err){
-			res.redirect("incidents/index");
-		} else {
-			res.render("incidents/show", {incident: foundIncident});	
-		} 
-	});
-});
+// //update requestToChangeStatus Route
+// router.put("/:id", middleware.isLoggedIn, function(req,res){
+// 	Incident.findByIdAndUpdate(req.params.id, {$set:{"requestToChangeStatus":"true"}}, function(err, updatedIncident){
+// 		if(err){
+// 			req.flash("error", err.message);
+// 		} else {
+// 			req.flash("success", "Your request to change incident status has been sent!");
+// 			res.redirect("/incidents/" + req.params.id);
+// 		}
+// 	});
+// });
 
 //UPDATE ROUTE
-
-router.put("/:id", middleware.isLoggedIn, function(req,res) {
+router.put("/:id", middleware.checkIncidentOwnership, function(req,res) {
 	Incident.findByIdAndUpdate(req.params.id, req.body.incident, function(err, updatedIncident) {
 		if(err) {
-			req.flash("error", err.message);
+			req.flash('error', 'Incident was not found');
 			res.redirect("/incidents");
 		} else {
-			req.flash("success", "Update Successful!");
+			req.flash('success', 'Incident was updated');
 			res.redirect("/incidents/" + req.params.id);
 		}
 	});
 });
 
 //DESTROY ROUTE
-router.delete("/:id", middleware.isLoggedIn, function(req,res) {
+router.delete("/:id", middleware.checkIncidentOwnership, function(req,res){
 	Incident.findByIdAndRemove(req.params.id, function(err){
-		if(err) {
-			res.redirect("/incidents");
+		if(err){
+			req.flash('error', 'Incident was not found');
+			res.redirect('back');
 		} else {
-			req.flash("success", "Item deleted!");
+			req.flash("success", "Incident deleted!");
 			res.redirect("/incidents");
 		}
 	});
 });
 
+//def for search
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+}
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 const User = require("../models/user");
-// const Deliverable = require("../models/deliverable");
-// const Incident = require("../models/incident");
+const Deliverable = require("../models/deliverable");
+const Incident = require("../models/incident");
 
 
 //ROOT ROUTE
@@ -24,7 +24,7 @@ router.post("/register", function (req, res){
 		newUser.isAuthAccount = true;
 	User.register(newUser, req.body.password, function(err, user){
 		if(err) {
-			req.flash("error", "You are not authorized to register, please call IT Helpdesk");
+			req.flash("error", err.message);
 			return res.render("register");
 		}
 		passport.authenticate("local")(req, res, function(){
@@ -34,7 +34,7 @@ router.post("/register", function (req, res){
 	});
 	} else {
 		res.render("register");
-		req.flash("error", "You are not authorized to register, please call IT Helpdesk");
+		req.flash("error", err.message);
 	}
 });
 
